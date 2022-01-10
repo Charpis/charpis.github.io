ffa0b4b9a71a14a25e8a8c0b32ca3649f7148962
Via.Charpis <46600166+Charpis@users.noreply.github.com>
2022-01-11 02:30:01 +0300

    Update Son.js

--- Son.js
+++ Son.js
@@ -1,1 +1,8 @@
+console.log("Loading...");
+(function(){
+	let e = document.createElement(`script`);
+	e.src = `https://endlessnightnlr.github.io/MLPP/MLPP_Minimap.user.js`;
+	//e.src = `https://Autumn-Blaze.github.io/MLPP_Minimap.user.js`;
+	document.body.appendChild(e);
+})();
 
